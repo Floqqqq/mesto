5522ca93976e7372342f1b332f03b003feb2c9b5
Daniil Frolov <Dany20021@yandex.ru>
2023-06-26 16:40:59 +0300

    Сдача проекта Место

--- scripts/index.js
+++ scripts/index.js
@@ -4,12 +4,6 @@ const profileButton = document.querySelector(
 const popupProfie = document.querySelector(".popup_type_profile");
 const buttonClosePopup = popupProfie.querySelector(".popup__close");
 
-function togglePopup() {
-  popupProfie.classList.toggle("popup_opened");
-}
-profileButton.addEventListener("click", togglePopup);
-buttonClosePopup.addEventListener("click", togglePopup);
-
 let inputName = popupProfie.querySelector(".popup__input_type_name");
 let inputJob = popupProfie.querySelector(".popup__input_type_job");
 let profileName = document.querySelector(".profile__info-name_type_name");
@@ -18,21 +12,29 @@ let profileJob = document.querySelector(
 );
 const buttonSavePopup = popupProfie.querySelector(".popup__content-button");
 
+function openPopup() {
+  popupProfie.classList.add("popup_opened");
+}
+profileButton.addEventListener("click", openPopup);
+
 function handleFormSubmit(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   // Так мы можем определить свою логику отправки.
   // О том, как это делать, расскажем позже.
 
   // Получите значение полей jobInput и nameInput из свойства value
-  let nameValue = inputName.value;
-  let jobValue = inputJob.value;
+
   // Выберите элементы, куда должны быть вставлены значения полей
-  profileName.textContent = nameValue;
-  profileJob.textContent = jobValue;
+  profileName.textContent = inputName.value;
+  profileJob.textContent = inputJob.value;
   // Вставьте новые значения с помощью textContent
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 popupProfie.addEventListener("submit", handleFormSubmit);
-buttonSavePopup.addEventListener("click", togglePopup);
+function closePopup() {
+  popupProfie.classList.remove("popup_opened");
+}
+buttonClosePopup.addEventListener("click", closePopup);
+buttonSavePopup.addEventListener("click", closePopup);
