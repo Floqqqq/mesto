1125dc8955451c30929b39715f8e9ee3cff429e3
Daniil Frolov <Dany20021@yandex.ru>
2023-06-28 01:02:05 +0300

    Сдача проекта Место

--- scripts/index.js
+++ scripts/index.js
@@ -2,20 +2,29 @@ const profileButton = document.querySelector(
   ".profile__edit-button_type_profile"
 );
 const popupProfie = document.querySelector(".popup_type_profile");
+const contentButton = popupProfie.querySelector(".popup__content-button");
 const buttonClosePopup = popupProfie.querySelector(".popup__close");
 
-let inputName = popupProfie.querySelector(".popup__input_type_name");
-let inputJob = popupProfie.querySelector(".popup__input_type_job");
-let profileName = document.querySelector(".profile__info-name_type_name");
-let profileJob = document.querySelector(
+const inputName = popupProfie.querySelector(".popup__input_type_name");
+const inputJob = popupProfie.querySelector(".popup__input_type_job");
+const profileName = document.querySelector(".profile__info-name_type_name");
+const profileJob = document.querySelector(
   ".profile__info-activity_type_activity"
 );
-const buttonSavePopup = popupProfie.querySelector(".popup__content-button");
+
+const editForm = popupProfie.querySelector('.edit-form')
 
 function openPopup() {
+  inputName.value = profileName.textContent;
+  inputJob.value = profileJob.textContent;
   popupProfie.classList.add("popup_opened");
 }
 profileButton.addEventListener("click", openPopup);
+function closePopup() {
+  popupProfie.classList.remove("popup_opened");
+}
+buttonClosePopup.addEventListener("click", closePopup);
+
 
 function handleFormSubmit(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
@@ -28,13 +37,9 @@ function handleFormSubmit(evt) {
   profileName.textContent = inputName.value;
   profileJob.textContent = inputJob.value;
   // Вставьте новые значения с помощью textContent
+  closePopup()
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-popupProfie.addEventListener("submit", handleFormSubmit);
-function closePopup() {
-  popupProfie.classList.remove("popup_opened");
-}
-buttonClosePopup.addEventListener("click", closePopup);
-buttonSavePopup.addEventListener("click", closePopup);
+editForm.addEventListener("submit", handleFormSubmit);
