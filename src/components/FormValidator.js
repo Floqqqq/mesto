dc1fcb28c522e418b6c86efb98f53953bf844ff9
Daniil Frolov <Dany20021@yandex.ru>
2023-09-06 23:33:12 +0300

    Сдача ПР8

---
+++ src/components/FormValidator.js
@@ -0,0 +1,76 @@
+export default class FormValidator {
+  constructor(config, templateSelector) {
+    this._config = config;
+    this._templateSelector = templateSelector;
+    this._formSelector = config.formSelector;
+    this._inputSelector = config.inputSelector;
+    this._submitButtonSelector = config.submitButtonSelector;
+    this._inactiveButtonClass = config.inactiveButtonClass;
+    this._inputErrorClass = config.inputErrorClass;
+    this._errorClass = config.errorClass;
+    this._form = document.querySelector(this._templateSelector);
+  }
+  _setEventListeners() {
+    this._inputs = Array.from(this._form.querySelectorAll(this._inputSelector));
+    this._button = this._form.querySelector(this._submitButtonSelector);
+
+    this._toggleButtonState();
+
+    this._inputs.forEach((input) => {
+      input.addEventListener("input", () => {
+        this._checkInputValidity(input);
+        this._toggleButtonState();
+      });
+    });
+  }
+  enableValidation() {
+    this._selectorForm = document.querySelector(this._formSelector);
+    this._selectorForm.addEventListener("submit", function (evt) {
+      evt.preventDefault();
+    });
+    this._setEventListeners();
+  }
+  _showInputError(input) {
+    this._span = this._form.querySelector(`.${input.id}-error`);
+    input.classList.add(this._inputErrorClass);
+    this._span.textContent = input.validationMessage;
+    this._span.classList.add(this._errorClass);
+  }
+
+  _hideInputError(input) {
+    this._span = this._form.querySelector(`.${input.id}-error`);
+    input.classList.remove(this._inputErrorClass);
+    this._span.textContent = "";
+    this._span.classList.remove(this._errorClass);
+  }
+
+  _checkInputValidity(input) {
+    !input.validity.valid
+      ? this._showInputError(input)
+      : this._hideInputError(input);
+  }
+
+  _hasInvalidInput() {
+    return this._inputs.some((input) => {
+      return !input.validity.valid;
+    });
+  }
+  inactiveButton() {
+    this._button.classList.add(this._inactiveButtonClass);
+    this._button.disabled = true;
+  }
+  _toggleButtonState() {
+    if (this._hasInvalidInput()) {
+      this.inactiveButton();
+    } else {
+      this._button.classList.remove(this._inactiveButtonClass);
+      this._button.disabled = false;
+    }
+  }
+  resetValidator() {
+    this._toggleButtonState();
+    this._inputs.forEach((input) => {
+      this._hideInputError(input);
+    });
+  }
+}
---
+++ src/pages/index.js
@@ -0,0 +1,88 @@
+import "./index.css";
+import {
+  initialCards,
+  config,
+  buttonOpenPopupProfile,
+  popupProfile,
+  inputName,
+  inputJob,
+  profileName,
+  profileJob,
+  popupPublication,
+  elements,
+  buttonOpenPopupPublication,
+  popupImageFullScreen,
+} from "../../utils/constants.js";
+import FormValidator from "../components/FormValidator.js";
+import Section from "../components/Section.js";
+import Card from "../components/Card.js";
+import Popup from "../components/Popup.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import UserInfo from "../components/UserInfo.js";
+const validateEditFormPublication = new FormValidator(
+  config,
+  ".popup__form_type_publication"
+);
+const validateFormProfile = new FormValidator(config, ".popup__form");
+validateFormProfile.enableValidation();
+validateEditFormPublication.enableValidation();
+
+const generateCard = (item) => {
+  const card = new Card(item, ".elemet-template", (name, link) => {
+    popupImage.open(name, link);
+  });
+  const cardElement = card.generateCard();
+  return cardElement;
+};
+
+const cardList = new Section(
+  {
+    items: initialCards,
+    renderer: (data) => {
+      const cardElement = generateCard(data);
+      cardList.addItem(cardElement);
+    },
+  },
+  elements
+);
+const popupImage = new PopupWithImage(popupImageFullScreen);
+popupImage.setEventListeners();
+
+const popupFormProfile = new PopupWithForm({
+  popup: popupProfile,
+  handleSubmitForm: (data) => {
+    userInfo.setUserInfo(data);
+    validateFormProfile.resetValidator();
+    popupFormProfile.close();
+  },
+});
+popupFormProfile.setEventListeners();
+const userInfo = new UserInfo({
+  userName: profileName,
+  userInfo: profileJob,
+});
+
+buttonOpenPopupProfile.addEventListener("click", () => {
+  const formValues = userInfo.getUserInfo();
+  popupFormProfile.open();
+  inputJob.value = formValues.info;
+  inputName.value = formValues.name;
+});
+
+const popupFormPublication = new PopupWithForm({
+  popup: popupPublication,
+  handleSubmitForm: (data) => {
+    const cardElement = generateCard(data);
+    cardList.addNewItem(cardElement);
+    popupFormPublication.close();
+    validateEditFormPublication.resetValidator();
+  },
+});
+popupFormPublication.setEventListeners();
+
+buttonOpenPopupPublication.addEventListener("click", () => {
+  popupFormPublication.open();
+});
+cardList.renderer();
+//
